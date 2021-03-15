1b3e5dc463e3e805de7f3b70727e41dd2f7a60a4
robertelias <robert.elias@live.com>
2021-03-15 15:57:51 -0700

    completed cancel button on view employee details mvp met on employee managment app

--- react-frontend/src/components/ViewEmployeeComponent.jsx
+++ react-frontend/src/components/ViewEmployeeComponent.jsx
@@ -11,37 +11,49 @@ class ViewEmployeeComponent extends Component {
     };
   }
 
+  cancel () {
+    this.props.history.push ('/employees');
+  }
   componentDidMount () {
     EmployeeService.getEmployeeById (this.state.id).then (res => {
       this.setState ({employee: res.data});
     });
   }
 
-   render() {
-        return (
-            <div>
-                <br></br>
-                <div className = "card col-md-6 offset-md-3">
-                    <h3 className = "text-center"> View Employee Details</h3>
-                    <div className = "card-body">
-                        <div className = "row">
-                            <label> Employee First Name: </label>
-                            <div> { this.state.employee.firstName }</div>
-                        </div>
-                        <div className = "row">
-                            <label> Employee Last Name: </label>
-                            <div> { this.state.employee.lastName }</div>
-                        </div>
-                        <div className = "row">
-                            <label> Employee Email ID: </label>
-                            <div> { this.state.employee.emailId }</div>
-                        </div>
-                    </div>
-
-                </div>
+  render () {
+    return (
+      <div>
+        <br />
+        <div className="card col-md-6 offset-md-3">
+          <h3 className="text-center"> View Employee Details</h3>
+          <div className="card-body">
+            <div className="row">
+              <label> Employee First Name: </label>
+              <div> {this.state.employee.firstName}</div>
             </div>
-        )
-    }
+            <div className="row">
+              <label> Employee Last Name: </label>
+              <div> {this.state.employee.lastName}</div>
+            </div>
+            <div className="row">
+              <label> Employee Email ID: </label>
+              <div> {this.state.employee.emailId}</div>
+            </div>
+          </div>
+
+          <button
+            className="btn btn-danger"
+            onClick={this.cancel.bind (this)}
+            style={{marginLeft: '-5px'}}
+          >
+            Cancel
+          </button>
+
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default ViewEmployeeComponent;
