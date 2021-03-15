601448f64d596be11f51cb69254c2cf7f1789a64
robertelias <robert.elias@live.com>
2021-03-15 15:48:45 -0700

    completed viewEmployee details component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,28 +1,35 @@
+import React from 'react';
+
 import './App.css';
-import HeaderComponent from './components/HeaderComponent';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
-import FooterComponent from './components/FooterComponent';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 import CreateEmployeeComponent from './components/CreateEmployeeComponent';
-import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
+//import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
 
 function App () {
   return (
     <div>
       <Router>
-        
-          <HeaderComponent />
-          <div className="Container">
-              <Switch> 
-              <Route path="/" exact component={ListEmployeeComponent}/>
-              <Route path="/employees" component={ListEmployeeComponent}/>
-              <Route path="/add-employee/:id" component={CreateEmployeeComponent}/>
-              <Route path="/view-employee/:id" component={UpdateEmployeeComponent}/>
-              
-                <ListEmployeeComponent />
-              </Switch>
-          </div>
-          <FooterComponent />        
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployeeComponent} />
+            <Route path="/employees" component={ListEmployeeComponent} />
+            <Route
+              path="/add-employee/:id"
+              component={CreateEmployeeComponent}
+            />
+            <Route
+              path="/view-employee/:id"
+              component={ViewEmployeeComponent}
+            />
+            {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+          </Switch>
+        </div>
+        <FooterComponent />
       </Router>
     </div>
   );
