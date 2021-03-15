8de238126ea6862fad5b9b1b995897353344f1d7
robertelias <robert.elias@live.com>
2021-03-14 20:58:18 -0700

    started routes form ListEmployeeComponent

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import FooterComponent from './components/FooterComponent';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 function App () {
   return (
     <div>
-      <HeaderComponent />
-      <div className="Container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+        
+          <HeaderComponent />
+          <div className="Container">
+              <Switch> 
+              <Route path="/" component={ListEmployeeComponent}></Route>
+              <Route path="/employees" component={ListEmployeeComponent} />
+                <ListEmployeeComponent />
+              </Switch>
+          </div>
+          <FooterComponent />        
+      </Router>
     </div>
   );
 }
